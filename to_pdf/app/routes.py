5566dc04be3759fcc3c40857243b9d9dee000b97
EricSchles <ericschles@gmail.com>
2014-11-13 08:49:45 -0800

    uploader finished

--- to_pdf/app/routes.py
+++ to_pdf/app/routes.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, make_response
 from werkzeug import secure_filename
+from parser import Parser
 
 UPLOAD_FOLDER = os.getcwd()
 ALLOWED_EXTENSIONS = set(['pdf'])
@@ -11,9 +12,37 @@ def allowed_files(filename):
 	return '.' in filename and \
 		filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
 
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+# 	return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+@app.route('/download/<filename>')
+def download(filename):
+	response = make_response(filename)
+	response.headers['Content-Disposition'] = "attachment; filename="+filename
+	return response
+
+
+
+
+
 @app.route("/",methods=['GET','POST'])
 def index():
 	if request.method == 'POST':
 		pdf_file = request.files['file']
-		if pdf_file and allowed_file(pdf_file.filename)
-		pdf_file.save(os.path.join)
+		if pdf_file and allowed_files(pdf_file.filename):
+			filename = secure_filename(pdf_file.filename)
+			pdf_file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+			
+
+
+			return redirect(url_for('download',filename=filename))
+	return '''
+	<!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
