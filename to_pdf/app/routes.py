69b37fc8943ce43cbdfeeefc80a3d2a840dfe37c
EricSchles <ericschles@gmail.com>
2014-11-13 08:25:09 -0800

    adding routes

--- to_pdf/app/routes.py
+++ to_pdf/app/routes.py
@@ -1,10 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
 
 UPLOAD_FOLDER = os.getcwd()
 ALLOWED_EXTENSIONS = set(['pdf'])
 
 app = Flask(__name__)
 
-@app.route("/")
+def allowed_files(filename):
+	return '.' in filename and \
+		filename.rsplit('.',1)[1] in ALLOWED_EXTENSIONS
+
+@app.route("/",methods=['GET','POST'])
 def index():
+	if request.method == 'POST':
+		pdf_file = request.files['file']
+		if pdf_file and allowed_file(pdf_file.filename)
+		pdf_file.save(os.path.join)
